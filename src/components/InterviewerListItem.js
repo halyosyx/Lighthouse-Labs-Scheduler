f0e2ad46281938f540ea28e751b6ff05784ac593
Christian Aisle Neral <aisle.github@gmail.com>
2021-12-14 09:28:39 -0800

    Implemented various functionalities

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import 'components/InterviewerListItem.scss';
+import classNames from 'classnames';
+
+
+export default function InterviewerListItem(props){
+
+    const interviewClass = classNames("interviewers__item",
+    {"interviewers__item--selected": props.selected
+    })
+
+    const imageClass = classNames("interviewers__item-image",
+    {"interviewers__item--selected-image": props.selected
+    })
+
+    return(
+    <li className={interviewClass} onClick={props.setInterviewer}>
+        <img
+            className={imageClass}
+            src={props.avatar}
+            alt={props.name}
+        />
+        {props.selected && props.name}
+    </li>
+    )
+}
