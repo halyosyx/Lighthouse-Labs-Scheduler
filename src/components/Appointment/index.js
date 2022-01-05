d8bbd81624fcd0412a61ce85818057cf1b9f8acc
Christian Aisle Neral <aisle.github@gmail.com>
2022-01-04 22:10:13 -0800

    Fixed select interview hang bug and select bug

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -85,7 +85,7 @@ export default function Appointment(props) {
         {mode === EDIT && (
           <Form
             name={props.name ? props.name : props.interview.student}
-            interviewer={props.interview.interviewer}
+            interviewer={props.interviewer}
             interviewers={props.interviewers}
             onSave={save}
             onCancel={back}
